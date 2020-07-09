4711e1c5551054cc1187d57a4f4ad28f14aa5149
windweller <leo.niecn@gmail.com>
2020-07-09 10:26:48 -0700

    a

--- autograde/train/dqn_pixel_agent.py
+++ autograde/train/dqn_pixel_agent.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 from stable_baselines import DQN
 from stable_baselines.common.vec_env import VecFrameStack, DummyVecEnv
 from stable_baselines.common.vec_env.subproc_vec_env import SubprocVecEnv
-from stable_baselines.common.atari_wrappers import ScaledFloatFrame, WarpFrame
+from stable_baselines.common.atari_wrappers import ScaledFloatFrame, WarpFrame, MaxAndSkipEnv
 
 from stable_baselines.common.callbacks import CallbackList, EvalCallback, CheckpointCallback, \
     StopTrainingOnRewardThreshold
@@ -78,39 +78,39 @@ class GrayScaleFrame(gym.ObservationWrapper):
         return frame
 
 
-class MaxAndSkipEnv(gym.Wrapper):
-    """
-    Wrapper from Berkeley's Assignment
-    Takes a max pool over the last n states
-    """
-
-    def __init__(self, env=None, skip=4):
-        """Return only every `skip`-th frame"""
-        super(MaxAndSkipEnv, self).__init__(env)
-        # most recent raw observations (for max pooling across time steps)
-        self._obs_buffer = deque(maxlen=2)
-        self._skip = skip
-
-    def step(self, action):
-        total_reward = 0.0
-        done = None
-        for _ in range(self._skip):
-            obs, reward, done, info = self.env.step(action)
-            self._obs_buffer.append(obs)
-            total_reward += reward
-            if done:
-                break
-
-        max_frame = np.max(np.stack(self._obs_buffer), axis=0)
-
-        return max_frame, total_reward, done, info
-
-    def reset(self):
-        """Clear past frame buffer and init. to first obs. from inner env."""
-        self._obs_buffer.clear()
-        obs = self.env.reset()
-        self._obs_buffer.append(obs)
-        return obs
+# class MaxAndSkipEnv(gym.Wrapper):
+#     """
+#     Wrapper from Berkeley's Assignment
+#     Takes a max pool over the last n states
+#     """
+#
+#     def __init__(self, env=None, skip=4):
+#         """Return only every `skip`-th frame"""
+#         super(MaxAndSkipEnv, self).__init__(env)
+#         # most recent raw observations (for max pooling across time steps)
+#         self._obs_buffer = deque(maxlen=2)
+#         self._skip = skip
+#
+#     def step(self, action):
+#         total_reward = 0.0
+#         done = None
+#         for _ in range(self._skip):
+#             obs, reward, done, info = self.env.step(action)
+#             self._obs_buffer.append(obs)
+#             total_reward += reward
+#             if done:
+#                 break
+#
+#         max_frame = np.max(np.stack(self._obs_buffer), axis=0)
+#
+#         return max_frame, total_reward, done, info
+#
+#     def reset(self):
+#         """Clear past frame buffer and init. to first obs. from inner env."""
+#         self._obs_buffer.clear()
+#         obs = self.env.reset()
+#         self._obs_buffer.append(obs)
+#         return obs
 
 
 def get_env_fn(program, reward_type, reward_shaping):
