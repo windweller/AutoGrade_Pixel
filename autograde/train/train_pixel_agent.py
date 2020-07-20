92243a5e21248833b4132ffbb206b47c8eb0bed2
windweller <leo.niecn@gmail.com>
2020-07-20 12:46:26 -0700

    update eval

--- autograde/train/train_pixel_agent.py
+++ autograde/train/train_pixel_agent.py
@@ -5,11 +5,13 @@ from stable_baselines import PPO2
 from stable_baselines.common.vec_env import VecFrameStack, DummyVecEnv
 from stable_baselines.common.vec_env.subproc_vec_env import SubprocVecEnv
 from stable_baselines.common.atari_wrappers import ScaledFloatFrame, WarpFrame
+from stable_baselines.common.vec_env import VecEnv
+
+import numpy as np
 from gym.wrappers import TimeLimit
 
 from stable_baselines.common.callbacks import CallbackList, EvalCallback, CheckpointCallback, StopTrainingOnRewardThreshold
 from stable_baselines.common.policies import CnnLstmPolicy
-from stable_baselines.common.evaluation import evaluate_policy
 
 from autograde.rl_envs.bounce_env import BouncePixelEnv, Program, ONLY_SELF_SCORE, SELF_MINUS_HALF_OPPO
 from autograde.rl_envs.wrappers import ResizeFrame
@@ -48,6 +50,65 @@ def make_general_env(program, frame_stack, num_envs, reward_type, reward_shaping
 
     return env
 
+def evaluate_ppo_policy(model, env, n_training_envs, n_eval_episodes=10, deterministic=True,
+                    render=False, callback=None, reward_threshold=None,
+                    return_episode_rewards=False):
+    """
+    Runs policy for `n_eval_episodes` episodes and returns average reward.
+    This is made to work only with one env.
+
+    :param model: (BaseRLModel) The RL agent you want to evaluate.
+    :param env: (gym.Env or VecEnv) The gym environment. In the case of a `VecEnv`
+        this must contain only one environment.
+    :param n_eval_episodes: (int) Number of episode to evaluate the agent
+    :param deterministic: (bool) Whether to use deterministic or stochastic actions
+    :param render: (bool) Whether to render the environment or not
+    :param callback: (callable) callback function to do additional checks,
+        called after each step.
+    :param reward_threshold: (float) Minimum expected reward per episode,
+        this will raise an error if the performance is not met
+    :param return_episode_rewards: (bool) If True, a list of reward per episode
+        will be returned instead of the mean.
+    :return: (float, float) Mean reward per episode, std of reward per episode
+        returns ([float], [int]) when `return_episode_rewards` is True
+    """
+    if isinstance(env, VecEnv):
+        assert env.num_envs == 1, "You must pass only one environment when using this function"
+
+    episode_rewards, episode_lengths = [], []
+    for _ in range(n_eval_episodes):
+        obs = env.reset()
+        done, state = False, None
+        episode_reward = 0.0
+        episode_length = 0
+
+        zero_completed_obs = np.zeros((n_training_envs,) + env.observation_space.shape)
+        while not done:
+            # concatenate obs
+            # https://github.com/hill-a/stable-baselines/issues/166
+            zero_completed_obs[0, :] = obs
+
+            action, state = model.predict(obs, state=state, deterministic=deterministic)
+            obs, reward, done, _info = env.step(action[0])
+            episode_reward += reward
+            if callback is not None:
+                callback(locals(), globals())
+            episode_length += 1
+            if render:
+                env.render()
+        episode_rewards.append(episode_reward)
+        episode_lengths.append(episode_length)
+
+    mean_reward = np.mean(episode_rewards)
+    std_reward = np.std(episode_rewards)
+
+    if reward_threshold is not None:
+        assert mean_reward > reward_threshold, 'Mean reward below threshold: '\
+                                         '{:.2f} < {:.2f}'.format(mean_reward, reward_threshold)
+    if return_episode_rewards:
+        return episode_rewards, episode_lengths
+    return mean_reward, std_reward
+
 def main():
     program = Program()
     program.set_correct()
@@ -56,8 +117,6 @@ def main():
 
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True  # pylint: disable=E1101
-
-    # TODO: Next step plan: Add Monitor with episode reward and episode length!!!
 
     with tf.Session(config=config):
 
@@ -70,7 +129,7 @@ def main():
 
         # Eval first to make sure we can eval this...(otherwise there's no point in training...)
         single_env = make_general_env(program, 1, 1, SELF_MINUS_HALF_OPPO, reward_shaping=False)
-        mean_reward, std_reward = evaluate_policy(model, single_env, n_eval_episodes=10)
+        mean_reward, std_reward = evaluate_ppo_policy(model, single_env, n_training_envs=8, n_eval_episodes=10)
 
         print("initial model mean reward {}, std reward {}".format(mean_reward, std_reward))
 
@@ -84,7 +143,7 @@ def main():
         single_env = make_general_env(program, 1, 1, SELF_MINUS_HALF_OPPO, reward_shaping=False)
         # AssertionError: You must pass only one environment when using this function
         # But then, the NN is expecting shape of (8, ...)
-        mean_reward, std_reward = evaluate_policy(model, single_env, n_eval_episodes=10)
+        mean_reward, std_reward = evaluate_ppo_policy(model, single_env, n_training_envs=8, n_eval_episodes=10)
         print("final model mean reward {}, std reward {}".format(mean_reward, std_reward))
 
         env.close()
