b8e880689d6ff3eadd49f78444219439d60c2e9d
windweller <leo.niecn@gmail.com>
2020-09-09 12:22:17 -0700

    add tail sampling

--- autograde/video/grade.py
+++ autograde/video/grade.py
@@ -93,6 +93,82 @@ def evaluate_on_rewards_and_values(n, skip):
 
     print("Time took {} secs".format(time.time() - start))
 
+def evaluate_on_tail():
+    # we evaluate on sampled 1000 tail programs
+
+    wandb.init(project="autograde-rollout", name="1000_sampled_tail_uniq_programs")
+
+    model_file = pjoin(pathlib.Path(__file__).parent.parent.absolute(),
+                       "train/saved_models/bounce_ppo2_cnn_lstm_one_ball_mixed_theme/ppo2_cnn_lstm_default_mixed_theme_final.zip")
+
+    rlc = RLController(model_file, n_train_env=8)
+
+    rlc.load_model()
+
+    program_folder = pjoin(pathlib.Path(__file__).parent.parent.absolute(), "envs/bounce_programs")
+
+    save_stats_dir = './eval_reward_value_stats_1000_sampled_tail/'
+    os.makedirs(save_stats_dir, exist_ok=True)
+
+    start = time.time()
+
+    prog = 0
+    for uniq_program_loc in tqdm(
+            glob.glob(pjoin(program_folder, "1000_sampled_tail_uniq_programs", "correct", "*.json"))):
+        avg_reward, rewards, step_rewards, step_values, step_dones = rlc.play_program(uniq_program_loc, 8,
+                                                                                      return_stats=True)
+        rew_str = ",".join([str(r) for r in rewards]) + '\n'
+        f = open(save_stats_dir + 'correct_{}_rewards.txt'.format(uniq_program_loc.split('/')[-1].rstrip(".json")),
+                 'w')
+        f.write(rew_str)
+        f.close()
+        # save other stats
+        filename = save_stats_dir + 'correct_{}_info.json'.format(uniq_program_loc.split('/')[-1].rstrip(".json"))
+        f = open(filename, 'w')
+        json.dump({'step_rewards': step_rewards,
+                   'step_values': step_values,
+                   'step_dones': step_dones}, f)
+        f.close()
+
+        prog += 1
+        time_so_far = time.time() - start
+        time_per_program = time_so_far / prog
+        estimated_time_to_complete = time_per_program * (1000 - prog)
+
+        wandb.log({"Estimated Time to Complete (secs)": estimated_time_to_complete,
+                   "Estimated Time to Complete (min)": estimated_time_to_complete / 60,
+                   "Spent time (secs)": time_so_far})
+
+    for uniq_program_loc in tqdm(
+            glob.glob(pjoin(program_folder, "1000_sampled_tail_uniq_programs", "broken", "*.json"))):
+        avg_reward, rewards, step_rewards, step_values, step_dones = rlc.play_program(uniq_program_loc, 8,
+                                                                                      return_stats=True)
+        rew_str = ",".join([str(r) for r in rewards]) + '\n'
+        f = open(save_stats_dir + 'broken_{}_rewards.txt'.format(uniq_program_loc.split('/')[-1].rstrip(".json")),
+                 'w')
+        f.write(rew_str)
+        f.close()
+        # save other stats
+        filename = save_stats_dir + 'broken_{}_info.json'.format(uniq_program_loc.split('/')[-1].rstrip(".json"))
+        f = open(filename, 'w')
+        json.dump({'step_rewards': step_rewards,
+                   'step_values': step_values,
+                   'step_dones': step_dones}, f)
+
+        # shape: (1, 1000, 8) -- if put in Numpy
+        f.close()
+
+        prog += 1
+        time_so_far = time.time() - start
+        time_per_program = time_so_far / prog
+        estimated_time_to_complete = time_per_program * (1000 - prog)
+
+        wandb.log({"Estimated Time to Complete (secs)": estimated_time_to_complete,
+                   "Estimated Time to Complete (min)": estimated_time_to_complete / 60,
+                   "Spent time (secs)": time_so_far})
+
+    print("Time took {} secs".format(time.time() - start))
+
 def setup_theme_json_string(scene, ball, paddle):
     from string import Template
 
@@ -253,6 +329,8 @@ def gen_traj_for_reference_broken_program_rewards_and_values():
 
 
 if __name__ == '__main__':
-    run_evaluate_on_rewards_and_values()
+    pass
+    # run_evaluate_on_rewards_and_values()
     # gen_traj_for_correct_program_rewards_and_values()
     # gen_traj_for_reference_broken_program_rewards_and_values()
+    evaluate_on_tail()
